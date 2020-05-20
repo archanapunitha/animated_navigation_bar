394b0215924ad214c227f07c2531cfa89a92dc44
Archana <62561163+archanapunitha@users.noreply.github.com>
2020-05-20 14:29:45 +0530

    Designed Home state commit

--- lib/Home.dart
+++ lib/Home.dart
@@ -3,6 +3,16 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Container(
+        child: Text("Home page",
+        style: TextStyle(
+          color: Colors.deepPurpleAccent,
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+        ),),
+        padding: EdgeInsets.all(10.0),
+      ),
+    );
   }
 }
