7e073c81a2b658f3595c9df648e76f8d17f44aee
Archana <62561163+archanapunitha@users.noreply.github.com>
2020-05-20 14:32:10 +0530

    update foody cart designed

--- lib/FoodyCart.dart
+++ lib/FoodyCart.dart
@@ -4,7 +4,15 @@ class FoodyCart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      
+      child: Container(
+        child: Text("Foody Cart Page",
+          style: TextStyle(
+            color: Colors.deepPurpleAccent,
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),),
+        padding: EdgeInsets.all(10.0),
+      ),
     );
   }
 }
