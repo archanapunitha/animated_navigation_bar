3e52f94b009278b677855f25934c31d84627c4fa
Archana <62561163+archanapunitha@users.noreply.github.com>
2020-05-20 14:30:34 +0530

    update weather page

--- lib/Weather.dart
+++ lib/Weather.dart
@@ -3,6 +3,16 @@ import 'package:flutter/material.dart';
 class Weather extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Container(
+        child: Text("weather Page",
+          style: TextStyle(
+            color: Colors.deepPurpleAccent,
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),),
+        padding: EdgeInsets.all(10.0),
+      ),
+    );
   }
 }
