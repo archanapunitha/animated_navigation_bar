5f98d2c169a5ae62b31f9b3bf8753ca52629464b
Archana <62561163+archanapunitha@users.noreply.github.com>
2020-05-20 14:31:29 +0530

    Foody Cart orientation changed

--- lib/FoodyCart.dart
+++ lib/FoodyCart.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class FoodyCart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      
+    );
   }
 }
